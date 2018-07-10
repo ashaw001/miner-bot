2e74d272ed6f90829fb5bd49838f2c95ab8fc078
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-07-10 10:16:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,21 +117,36 @@ var routineD = setTimeout(function() {routine4(1)}, 241000);
 	var initial1 = setTimeout(function() {strt(1)}, 200);
 	var initial2 = setInterval(function() {strt(1)}, 480000);
  }
-if (message.author.id == 272868091028045824 && message.channel.id == 313400507743862794) {
-function upvote1 () {
-	message.react("335141910773628928");}
-var upvote = setTimeout(function() {upvote1(1)}, 240000);
+if (message.author.id == 272868091028045824 && message.content.includes("swanrollcall")) {
+function rollcall () {
+	message.channel.send('Mr @dino_inc#0817 
+Mr @lyricballoon80#7386 
+Mr @Kompressed#8524 
+Mr @Arsole#1968 
+Mr @[BBB] President Daddy Trump#9809
+Mr @Rogue#6969 
+Mr @Spaghettiman#2183 
+Mr @Liar, The Jew#5720 
+Mr @KΞЯI$VΞCŦ | KEVE#2998 
+Mr @Rug#1174 
+Mr @iamflabbergasted#9951 
+Mr @felface#9652 
+Mr @Jim#5280 
+Mr @heretic#3684 
+Mr @Lucky#1661 
+Mr @Air Bud For 2020#2438 
+Mr @thief#2737 
+Mr @MIP5#4383 
+Mr @chungus#1029 
+Mr @Evan#1391 
+Mr @Swan (Redacted) Ronson#1991
+Mr @Tresh#9076 
+Mr @T.#6294 
+Mr @cfschris#7138
+Mr @Poison#5882');
+var rollcall = setTimeout(function() {rolcall(1)}, 4000);
 	} 
-if (message.author.id == 283353337292783616 && message.channel.id == 313400507743862794) {
-function upvote1 () {
-	message.react(":upvote:335141910773628928");}
-var upvote = setTimeout(function() {upvote1(1)}, 240000);
-	} 
-if (message.author.id == 225254925704101888 && message.channel.id == 313400507743862794) {
-function downvote1 () {
-	message.react(":downvote:335141916989456384");}
-var downvote = setTimeout(function() {downvote1(1)}, 240000);
-	}
+
 });
 //________________________________________________________________________________________________________________________
 //_______________________________________________________________________________________________________________________
