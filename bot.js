48267cda106dedfee261df80359440ee25244a05
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-07-10 10:24:48 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,34 +119,10 @@ var routineD = setTimeout(function() {routine4(1)}, 241000);
  }
 if (message.author.id == 144214136434327554 || message.author.id == 260241663018926080 && message.content.includes("swanrollcall")) {
 function rollcall () {
-	message.channel.send('Mr @dino_inc#0817 
-Mr @lyricballoon80#7386 
-Mr @Kompressed#8524 
-Mr @Arsole#1968 
-Mr @[BBB] President Daddy Trump#9809
-Mr @Rogue#6969 
-Mr @Spaghettiman#2183 
-Mr @Liar, The Jew#5720 
-Mr @KΞЯI$VΞCŦ | KEVE#2998 
-Mr @Rug#1174 
-Mr @iamflabbergasted#9951 
-Mr @felface#9652 
-Mr @Jim#5280 
-Mr @heretic#3684 
-Mr @Lucky#1661 
-Mr @Air Bud For 2020#2438 
-Mr @thief#2737 
-Mr @MIP5#4383 
-Mr @chungus#1029 
-Mr @Evan#1391 
-Mr @Swan (Redacted) Ronson#1991
-Mr @Tresh#9076 
-Mr @T.#6294 
-Mr @cfschris#7138
-Mr @Poison#5882');
-var rollcall = setTimeout(function() {rolcall(1)}, 4000);
-	} 
-
+	message.channel.send('Mr @dino_inc#0817 \n Mr @lyricballoon80#7386 \n Mr @Kompressed#8524 \n Mr @Arsole#1968 \n Mr @[BBB] President Daddy Trump#9809 \n Mr @Rogue#6969 \n Mr @Spaghettiman#2183 \n Mr @Liar, The Jew#5720 Mr @KΞЯI$VΞCŦ | KEVE#2998 Mr @Rug#1174 Mr @iamflabbergasted#9951 Mr @felface#9652 Mr @Jim#5280 Mr @heretic#3684 Mr @Lucky#1661 Mr @Air Bud For 2020#2438 Mr @thief#2737 Mr @MIP5#4383 Mr @chungus#1029 Mr @Evan#1391 Mr @Swan (Redacted) Ronson#1991 Mr @Tresh#9076 Mr @T.#6294 Mr @cfschris#7138 Mr @Poison#5882');
+	}
+	var rollcall = setTimeout(function() {rolcall(1)}, 4000);
+} 
 });
 //________________________________________________________________________________________________________________________
 //_______________________________________________________________________________________________________________________
